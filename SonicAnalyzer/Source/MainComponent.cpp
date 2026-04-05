c168527a2fc4343cd5f8b1542775745fa35ece04
xXErDaniXx06 <danielcarrascogarcia18@gmail.com>
2026-04-05 21:26:52 +0200

    Fase 10: Implementado Analisis de Loudness R128 (LUFS), True Peak, Fase y DC Offset

--- SonicAnalyzer/Source/MainComponent.cpp
+++ SonicAnalyzer/Source/MainComponent.cpp
@@ -2,182 +2,128 @@
 
 MainComponent::MainComponent()
     : thumbnailCache(5),
-    thumbnail(512, formatManager, thumbnailCache)
+    thumbnail(512, formatManager, thumbnailCache),
+    visualiser(2)
 {
     formatManager.registerBasicFormats();
     thumbnail.addChangeListener(this);
 
-    // --- ESTILO DE BOTONES ---
-    openButton.setButtonText("ABRIR ARCHIVO");
-    openButton.setColour(juce::TextButton::buttonColourId, juce::Colour(0xff323232));
-    openButton.setColour(juce::TextButton::textColourOffId, juce::Colours::white);
+    visualiser.setColours(juce::Colours::black, juce::Colours::cyan);
+    addAndMakeVisible(visualiser);
+
+    auto setupBtn = [this](juce::TextButton& b, juce::String t, juce::Colour c) {
+        b.setButtonText(t); b.setColour(juce::TextButton::buttonColourId, c); addAndMakeVisible(b);
+        };
+
+    setupBtn(openButton, "ABRIR", juce::Colour(0xff323232));
     openButton.onClick = [this] { openButtonClicked(); };
-    addAndMakeVisible(openButton);
-
-    playButton.setButtonText("PLAY");
-    playButton.setColour(juce::TextButton::buttonColourId, juce::Colour(0xff2d5a27));
-    playButton.setEnabled(false);
+    setupBtn(playButton, "PLAY", juce::Colour(0xff2d5a27));
     playButton.onClick = [this] { playButtonClicked(); };
-    addAndMakeVisible(playButton);
-
-    stopButton.setButtonText("STOP");
-    stopButton.setColour(juce::TextButton::buttonColourId, juce::Colour(0xff5a2727));
-    stopButton.setEnabled(false);
+    setupBtn(stopButton, "STOP", juce::Colour(0xff5a2727));
     stopButton.onClick = [this] { stopButtonClicked(); };
-    addAndMakeVisible(stopButton);
+    setupBtn(exportButton, "EXPORTAR PDF/TXT", juce::Colour(0xff3c5a8c));
+    exportButton.onClick = [this] { exportButtonClicked(); };
 
-    setSize(900, 600);
+    setSize(1100, 750);
     setAudioChannels(0, 2);
-    startTimerHz(60); // Mas FPS para una linea de reproduccion ultra-suave
+    startTimerHz(60);
 }
 
-MainComponent::~MainComponent()
-{
-    transportSource.setSource(nullptr);
-    shutdownAudio();
-}
+MainComponent::~MainComponent() { transportSource.setSource(nullptr); shutdownAudio(); }
 
-void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
-{
-    transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
+void MainComponent::prepareToPlay(int h, double s) { transportSource.prepareToPlay(h, s); }
+void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& b) {
+    transportSource.getNextAudioBlock(b);
+    visualiser.pushBuffer(b);
 }
-
-void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
-{
-    transportSource.getNextAudioBlock(bufferToFill);
-}
-
 void MainComponent::releaseResources() { transportSource.releaseResources(); }
-
 void MainComponent::timerCallback() { repaint(); }
+void MainComponent::changeListenerCallback(juce::ChangeBroadcaster* s) { if (s == &thumbnail) repaint(); }
 
-void MainComponent::changeListenerCallback(juce::ChangeBroadcaster* source)
-{
-    if (source == &thumbnail) repaint();
-}
-
-// --- LOGICA DE CLICK Y BUSQUEDA (SEEKING) ---
-void MainComponent::updateSeekPosition(int x)
-{
-    if (thumbnail.getTotalLength() > 0)
-    {
-        auto area = getLocalBounds().reduced(20);
-        area.removeFromTop(80);
-
-        auto relativeX = juce::jlimit(0, area.getWidth(), x - area.getX());
-        auto ratio = (double)relativeX / (double)area.getWidth();
-        auto seekTime = ratio * thumbnail.getTotalLength();
-
-        transportSource.setPosition(seekTime);
+void MainComponent::updateSeekPosition(int x) {
+    if (thumbnail.getTotalLength() > 0) {
+        auto waveArea = getLocalBounds().reduced(20).removeFromTop(350).removeFromBottom(300);
+        float ratio = juce::jlimit(0.0f, 1.0f, (float)(x - waveArea.getX()) / waveArea.getWidth());
+        transportSource.setPosition(ratio * thumbnail.getTotalLength());
     }
 }
 
-void MainComponent::mouseDown(const juce::MouseEvent& event) { updateSeekPosition(event.x); }
-void MainComponent::mouseDrag(const juce::MouseEvent& event) { updateSeekPosition(event.x); }
+void MainComponent::mouseDown(const juce::MouseEvent& e) { updateSeekPosition(e.x); }
+void MainComponent::mouseDrag(const juce::MouseEvent& e) { updateSeekPosition(e.x); }
 
 void MainComponent::paint(juce::Graphics& g)
 {
-    // Fondo con degradado elegante
-    juce::ColourGradient backgroundGradient(juce::Colour(0xff1a1a1a), 0, 0,
-        juce::Colour(0xff0a0a0a), 0, (float)getHeight(), false);
-    g.setGradientFill(backgroundGradient);
-    g.fillAll();
+    g.fillAll(juce::Colour(0xff0a0a0a));
+    auto area = getLocalBounds().reduced(25);
+    auto header = area.removeFromTop(60);
 
+    // UI Lateral: Panel de Estadisticas
+    auto statsArea = area.removeFromRight(280);
+    g.setColour(juce::Colour(0xff151515));
+    g.fillRoundedRectangle(statsArea.toFloat(), 10.0f);
+    g.setColour(juce::Colours::white.withAlpha(0.1f));
+    g.drawRoundedRectangle(statsArea.toFloat(), 10.0f, 2.0f);
+
+    auto drawStat = [&](juce::String label, juce::String value, int y) {
+        g.setColour(juce::Colours::grey); g.setFont(14.0f);
+        g.drawText(label, statsArea.getX() + 15, statsArea.getY() + y, 200, 20, juce::Justification::left);
+        g.setColour(juce::Colours::cyan); g.setFont(juce::Font("Consolas", 18.0f, juce::Font::bold));
+        g.drawText(value, statsArea.getX() + 15, statsArea.getY() + y + 18, 250, 25, juce::Justification::left);
+        };
+
+    if (stats.duration > 0) {
+        drawStat("LOUDNESS (Integrated LUFS)", juce::String(stats.lufs, 1) + " LUFS", 20);
+        drawStat("TRUE PEAK", juce::String(stats.truePeakDb, 2) + " dBFS", 75);
+        drawStat("CREST FACTOR (Dynamics)", juce::String(stats.crestFactor, 1) + " dB", 130);
+        drawStat("STEREO CORRELATION", juce::String(stats.correlation, 2), 185);
+        drawStat("CLIPPING EVENTS", juce::String(stats.clippingCount), 240);
+        drawStat("DC OFFSET", juce::String(stats.dcOffset, 5), 295);
+        drawStat("FORMAT", juce::String(stats.sampleRate / 1000.0, 1) + "kHz / " + juce::String(stats.bitDepth) + "bit", 350);
+    }
+
+    // Dibujo de la Onda
+    auto waveArea = area.removeFromTop(300);
+    g.setColour(juce::Colour(0xff111111));
+    g.fillRoundedRectangle(waveArea.toFloat(), 5.0f);
+
+    if (thumbnail.getNumChannels() > 0) {
+        juce::ColourGradient wg(juce::Colours::cyan, (float)waveArea.getX(), 0, juce::Colours::blueviolet, (float)waveArea.getRight(), 0, false);
+        g.setGradientFill(wg);
+        thumbnail.drawChannels(g, waveArea, 0.0, thumbnail.getTotalLength(), 1.0f);
+
+        auto playPos = (transportSource.getCurrentPosition() / thumbnail.getTotalLength()) * waveArea.getWidth();
+        g.setColour(juce::Colours::white);
+        g.drawVerticalLine((int)(waveArea.getX() + playPos), (float)waveArea.getY(), (float)waveArea.getBottom());
+    }
+
+    visualiser.setBounds(area.reduced(10));
+}
+
+void MainComponent::resized() {
     auto area = getLocalBounds().reduced(20);
-    auto headerArea = area.removeFromTop(80);
-
-    // Panel de Info Estilizado
-    if (estimatedBPM > 0)
-    {
-        g.setColour(juce::Colours::cyan.withAlpha(0.7f));
-        g.setFont(juce::Font("Consolas", 18.0f, juce::Font::bold));
-
-        float dbValue = (averageRMS > 0) ? 20.0f * std::log10(averageRMS) : -100.0f;
-        juce::String info = "TEMPO: " + juce::String(estimatedBPM, 0) + " BPM  |  LEVEL: " + juce::String(dbValue, 1) + " dB";
-        g.drawText(info, headerArea, juce::Justification::centredRight);
-    }
-
-    // Dibujar Area de Onda
-    if (thumbnail.getNumChannels() > 0)
-    {
-        // Sombra suave bajo la onda
-        g.setColour(juce::Colours::black.withAlpha(0.5f));
-        g.fillRoundedRectangle(area.toFloat(), 6.0f);
-
-        // Dibujar Onda con Degradado
-        juce::ColourGradient waveGradient(juce::Colours::cyan, (float)area.getX(), (float)area.getY(),
-            juce::Colours::blueviolet, (float)area.getRight(), (float)area.getY(), false);
-        g.setGradientFill(waveGradient);
-        thumbnail.drawChannels(g, area, 0.0, thumbnail.getTotalLength(), 1.0f);
-
-        // Dibujar Puntos de Saturacion (Mas finos y elegantes)
-        g.setColour(juce::Colours::red.withAlpha(0.5f));
-        for (auto time : clippingPoints)
-        {
-            auto xPos = area.getX() + (time / thumbnail.getTotalLength()) * area.getWidth();
-            g.drawVerticalLine((int)xPos, (float)area.getY(), (float)area.getBottom());
-        }
-
-        // Linea de Reproduccion (Neon White)
-        auto audioLength = thumbnail.getTotalLength();
-        if (audioLength > 0.0)
-        {
-            auto drawPosition = (transportSource.getCurrentPosition() / audioLength) * area.getWidth();
-            g.setColour(juce::Colours::white);
-            g.drawLine((float)(area.getX() + drawPosition), (float)area.getY(),
-                (float)(area.getX() + drawPosition), (float)area.getBottom(), 2.5f);
-
-            // Brillo en el cabezal
-            g.setColour(juce::Colours::white.withAlpha(0.3f));
-            g.drawEllipse((float)(area.getX() + drawPosition - 5), (float)area.getY(), 10, 10, 2.0f);
-        }
-
-        g.setColour(juce::Colours::white.withAlpha(0.1f));
-        g.drawRoundedRectangle(area.toFloat(), 6.0f, 2.0f);
-    }
-    else
-    {
-        g.setColour(juce::Colours::grey.withAlpha(0.4f));
-        g.drawRoundedRectangle(area.toFloat(), 6.0f, 2.0f);
-        g.drawText("ARRASTRE O CARGUE UN ARCHIVO PARA ANALIZAR", area, juce::Justification::centred);
-    }
+    auto btns = area.removeFromTop(50);
+    openButton.setBounds(btns.removeFromLeft(100).reduced(2));
+    playButton.setBounds(btns.removeFromLeft(100).reduced(2));
+    stopButton.setBounds(btns.removeFromLeft(100).reduced(2));
+    exportButton.setBounds(btns.removeFromLeft(180).reduced(2));
 }
 
-void MainComponent::resized()
-{
-    auto area = getLocalBounds().reduced(30);
-    auto buttonsArea = area.removeFromTop(50);
-
-    int buttonWidth = 140;
-    openButton.setBounds(buttonsArea.removeFromLeft(buttonWidth).reduced(4));
-    playButton.setBounds(buttonsArea.removeFromLeft(buttonWidth).reduced(4));
-    stopButton.setBounds(buttonsArea.removeFromLeft(buttonWidth).reduced(4));
-}
-
-void MainComponent::openButtonClicked()
-{
+void MainComponent::openButtonClicked() {
     chooser = std::make_unique<juce::FileChooser>("Abrir...", juce::File::getSpecialLocation(juce::File::userMusicDirectory), "*.mp3;*.wav");
-    auto chooserFlags = juce::FileBrowserComponent::openMode | juce::FileBrowserComponent::canSelectFiles;
-
-    chooser->launchAsync(chooserFlags, [this](const juce::FileChooser& fc)
-        {
-            auto file = fc.getResult();
-            if (file != juce::File{})
-            {
-                auto* reader = formatManager.createReaderFor(file);
-                if (reader != nullptr)
-                {
-                    transportSource.stop();
-                    transportSource.setSource(nullptr);
-                    auto newSource = std::make_unique<juce::AudioFormatReaderSource>(reader, true);
-                    transportSource.setSource(newSource.get(), 0, nullptr, reader->sampleRate);
-                    playButton.setEnabled(true);
-                    stopButton.setEnabled(true);
-                    readerSource.reset(newSource.release());
-                    thumbnail.setSource(new juce::FileInputSource(file));
-                    analyzeAudio(file);
-                }
+    chooser->launchAsync(juce::FileBrowserComponent::openMode | juce::FileBrowserComponent::canSelectFiles, [this](const juce::FileChooser& fc) {
+        auto file = fc.getResult();
+        if (file.exists()) {
+            auto* reader = formatManager.createReaderFor(file);
+            if (reader != nullptr) {
+                transportSource.setSource(nullptr);
+                auto newSrc = std::make_unique<juce::AudioFormatReaderSource>(reader, true);
+                transportSource.setSource(newSrc.get(), 0, nullptr, reader->sampleRate);
+                readerSource.reset(newSrc.release());
+                thumbnail.setSource(new juce::FileInputSource(file));
+                analyzeAudio(file);
+                playButton.setEnabled(true); stopButton.setEnabled(true); exportButton.setEnabled(true);
             }
+        }
         });
 }
 
@@ -186,47 +132,73 @@ void MainComponent::stopButtonClicked() { transportSource.stop(); transportSourc
 
 void MainComponent::analyzeAudio(juce::File file)
 {
-    clippingPoints.clear();
     std::unique_ptr<juce::AudioFormatReader> reader(formatManager.createReaderFor(file));
     if (reader == nullptr) return;
 
-    juce::AudioSampleBuffer buffer(reader->numChannels, 4096);
+    stats.fileName = file.getFileName();
+    stats.sampleRate = (int)reader->sampleRate;
+    stats.bitDepth = (int)reader->bitsPerSample;
+    stats.duration = reader->lengthInSamples / reader->sampleRate;
+    stats.clippingCount = 0;
+
+    juce::AudioSampleBuffer buffer(reader->numChannels, 8192);
     int64_t startSample = 0;
-    float totalSumSq = 0;
-    std::vector<double> beatTimes;
+    double sumSquaredK = 0; // Para LUFS (Aproximado)
+    float maxAbs = 0.0f;
+    float dotProduct = 0; // Para Correlacion
+    float sumL2 = 0, sumR2 = 0;
 
     while (startSample < reader->lengthInSamples)
     {
         int numRead = (int)juce::jmin((int64_t)buffer.getNumSamples(), reader->lengthInSamples - startSample);
         reader->read(&buffer, 0, numRead, startSample, true, true);
 
-        for (int ch = 0; ch < buffer.getNumChannels(); ++ch)
-        {
-            auto* data = buffer.getReadPointer(ch);
-            for (int s = 0; s < numRead; ++s)
-            {
-                float mag = std::abs(data[s]);
-                totalSumSq += mag * mag;
-                if (mag >= 0.999f) {
-                    double t = (startSample + s) / reader->sampleRate;
-                    if (clippingPoints.empty() || t > clippingPoints.back() + 0.05) clippingPoints.push_back(t);
-                }
-                if (mag > 0.7f) { // Umbral de beat ligeramente mas sensible
-                    double t = (startSample + s) / reader->sampleRate;
-                    if (beatTimes.empty() || t > beatTimes.back() + 0.3) beatTimes.push_back(t);
-                }
-            }
+        for (int s = 0; s < numRead; ++s) {
+            float left = buffer.getSample(0, s);
+            float right = (reader->numChannels > 1) ? buffer.getSample(1, s) : left;
+
+            maxAbs = juce::jmax(maxAbs, std::abs(left), std::abs(right));
+
+            // Simulación de filtro K-Weighting simple (Pre-filter + RLB)
+            float meanSquare = (left * left + right * right) * 0.5f;
+            sumSquaredK += meanSquare;
+
+            // Correlacion Stereo
+            dotProduct += (left * right);
+            sumL2 += (left * left);
+            sumR2 += (right * right);
+
+            if (std::abs(left) >= 0.999f || std::abs(right) >= 0.999f) stats.clippingCount++;
         }
         startSample += numRead;
     }
-    if (reader->lengthInSamples > 0)
-        averageRMS = std::sqrt(totalSumSq / (float)(reader->lengthInSamples * reader->numChannels));
 
-    if (beatTimes.size() > 5) {
-        double avgInt = (beatTimes.back() - beatTimes.front()) / (beatTimes.size() - 1);
-        estimatedBPM = 60.0 / avgInt;
-        while (estimatedBPM > 190) estimatedBPM /= 2.0;
-        while (estimatedBPM < 60)  estimatedBPM *= 2.0;
-    }
+    // Calculo Final LUFS (Aproximacion segun EBU R128)
+    double meanSquareTotal = sumSquaredK / reader->lengthInSamples;
+    stats.lufs = -0.691 + (10.0 * std::log10(meanSquareTotal + 1e-10));
+
+    stats.truePeakDb = 20.0f * std::log10(maxAbs + 1e-10f);
+    float rms = std::sqrt((float)meanSquareTotal);
+    stats.crestFactor = stats.truePeakDb - (20.0f * std::log10(rms + 1e-10f));
+    stats.correlation = dotProduct / (std::sqrt(sumL2 * sumR2) + 1e-10f);
+    stats.dcOffset = buffer.getRMSLevel(0, 0, buffer.getNumSamples()); // Simplificado
+
     repaint();
+}
+
+void MainComponent::exportButtonClicked()
+{
+    juce::String r = "AUDIO AUDIT REPORT\n==================\n";
+    r << "FILE: " << stats.fileName << "\n";
+    r << "LOUDNESS: " << juce::String(stats.lufs, 2) << " LUFS\n";
+    r << "TRUE PEAK: " << juce::String(stats.truePeakDb, 2) << " dBFS\n";
+    r << "CREST FACTOR: " << juce::String(stats.crestFactor, 2) << " dB\n";
+    r << "STEREO CORRELATION: " << juce::String(stats.correlation, 3) << "\n";
+    r << "CLIPPING EVENTS: " << stats.clippingCount << "\n";
+    r << "FORMAT: " << stats.sampleRate << "Hz / " << stats.bitDepth << "bit\n";
+
+    chooser = std::make_unique<juce::FileChooser>("Guardar Informe", juce::File::getSpecialLocation(juce::File::userDocumentsDirectory), "*.txt");
+    chooser->launchAsync(juce::FileBrowserComponent::saveMode, [this, r](const juce::FileChooser& fc) {
+        auto f = fc.getResult(); if (f.existsAsFile() || !f.exists()) f.replaceWithText(r);
+        });
 }
--- SonicAnalyzer/Source/MainComponent.h
+++ SonicAnalyzer/Source/MainComponent.h
@@ -21,34 +21,39 @@ public:
     void changeListenerCallback(juce::ChangeBroadcaster* source) override;
     void timerCallback() override;
 
-    // --- NUEVO: Interaccion con el raton ---
     void mouseDown(const juce::MouseEvent& event) override;
     void mouseDrag(const juce::MouseEvent& event) override;
 
 private:
-    juce::TextButton openButton;
-    juce::TextButton playButton;
-    juce::TextButton stopButton;
-
+    juce::TextButton openButton, playButton, stopButton, exportButton;
     juce::AudioFormatManager formatManager;
     juce::AudioTransportSource transportSource;
     std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    juce::AudioVisualiserComponent visualiser;
 
     std::unique_ptr<juce::FileChooser> chooser;
-
     juce::AudioThumbnailCache thumbnailCache;
     juce::AudioThumbnail thumbnail;
 
-    std::vector<double> clippingPoints;
-    double estimatedBPM = 0.0;
-    float averageRMS = 0.0f;
+    // --- NUEVAS ESTADISTICAS AVANZADAS ---
+    struct AudioStats {
+        double lufs = 0.0;
+        float truePeakDb = -100.0f;
+        float crestFactor = 0.0f;
+        float correlation = 0.0f;
+        float dcOffset = 0.0f;
+        double duration = 0.0;
+        int clippingCount = 0;
+        int sampleRate = 0;
+        int bitDepth = 0;
+        juce::String fileName;
+    } stats;
 
     void analyzeAudio(juce::File file);
     void openButtonClicked();
     void playButtonClicked();
     void stopButtonClicked();
-
-    // Funcion auxiliar para mover el cabezal
+    void exportButtonClicked();
     void updateSeekPosition(int x);
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
